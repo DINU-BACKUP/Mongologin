6a66f300cd889ee090ff2304beed5c0292f4501d
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2026-02-22 17:04:04 +0530

    Update login.js

--- api/login.js
+++ api/login.js
@@ -2,6 +2,7 @@ const { MongoClient } = require("mongodb");
 const { MONGO_URL } = require("../config");
 
 module.exports = async (req, res) => {
+
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Method not allowed" });
     }
@@ -16,7 +17,7 @@ module.exports = async (req, res) => {
     const user = await users.findOne({ username, password });
 
     if (!user) {
-        return res.status(400).json({ message: "Invalid username or password" });
+        return res.status(400).json({ message: "Invalid credentials" });
     }
 
     res.status(200).json({ message: "Login success" });
