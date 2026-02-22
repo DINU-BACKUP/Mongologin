fe66804a30e6cf21c9e9f79140d48cdead90cc29
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2026-02-22 22:41:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+// ====================
+// DARK / LIGHT MODE
+// ====================
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
+
+// Load saved theme
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+        document.body.classList.add("dark");
+    }
+});
 // GLOBAL ELEMENTS
 const ruser = document.getElementById("ruser");
 const rpass = document.getElementById("rpass");
