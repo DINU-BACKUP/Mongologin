0a7c53ffe5e13935649eaa45551a5f2c881591e6
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2026-02-22 17:33:01 +0530

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -8,23 +8,27 @@ module.exports = async (req, res) => {
     const db = client.db("dinuwhDB");
     const users = db.collection("users");
 
+    // ADMIN LOGIN + LOAD USERS
     if (req.method === "POST") {
 
         const { key } = req.body;
 
-        if (key !== ADMIN_KEY) {
+        if (key !== ADMIN_KEY)
             return res.status(400).json({ message: "Invalid Admin Key" });
-        }
 
         const allUsers = await users.find().toArray();
         return res.status(200).json({ users: allUsers });
     }
 
+    // ADMIN DELETE USER
     if (req.method === "DELETE") {
 
-        const { username } = req.body;
+        const { username, confirm } = req.body;
+
+        if (username !== confirm)
+            return res.status(400).json({ message: "Username confirmation failed" });
+
         await users.deleteOne({ username });
         return res.status(200).json({ message: "User deleted" });
     }
-
 };
