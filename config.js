b3596fab28ee71bcf5633e84063685e798391b9c
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2026-02-22 17:02:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
+
 module.exports = {
     MONGO_URL: "mongodb+srv://boveh30932_db_user:dwLCoaYvSa6Ue6W3@ape-amma.32impvs.mongodb.net/",
-    MAX_USERS: 50
+    MAX_USERS: 50,
+    ADMIN_KEY: "MANGALA123" // admin login special key
 };
