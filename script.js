6d8b56ff234badf2baa23f374968817568c852b6
DINU-BACKUP <hikunaherokuandgithub@gmail.com>
2026-02-22 17:20:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// THEME TOGGLE
+function toggleTheme() {
+    const body = document.body;
+
+    if (body.classList.contains("dark")) {
+        body.classList.remove("dark");
+        body.classList.add("light");
+        localStorage.setItem("theme", "light");
+    } else {
+        body.classList.remove("light");
+        body.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+    }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme") || "dark";
+    document.body.classList.add(savedTheme);
+});
 // REGISTER
 async function register() {
 
